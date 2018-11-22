0eca44c15dd223711189baf1763f944bbffb9478
shalevf6 <38422152+shalevf6@users.noreply.github.com>
2018-11-22 19:44:52 +0200

    UFC Implementation

--- src/UniformCostSearch.java
+++ src/UniformCostSearch.java
@@ -1,8 +1,13 @@
+import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+import java.util.Queue;
 
-public class UniformCostSearch   extends ASearch
+public class UniformCostSearch extends ASearch
 {
-	// Define lists here ...
-	
+	private PriorityQueue<ASearchNode> openList;
+	private Queue<ASearchNode> closedList;
+
 	@Override
 	public String getSolverName() 
 	{
@@ -10,64 +15,62 @@ public class UniformCostSearch   extends ASearch
 	}
 
 	@Override
-	public ASearchNode createSearchRoot
-	(
-		IProblemState problemState
-	) 
+	public ASearchNode createSearchRoot(IProblemState problemState	)
 	{
 		ASearchNode newNode = new BlindSearchNode(problemState);
 		return newNode;
 	}
 	
 	@Override
-	public void initLists() 
+	public void initLists()
 	{
-
+		Comparator<Object> comparator = new ASearchNodeComparator();
+		openList = new PriorityQueue(comparator);
+		closedList = new LinkedList<>();
 	}
 
 	@Override
-	public ASearchNode getOpen
-	(
-		ASearchNode node
-	) 
+	public ASearchNode getOpen(ASearchNode node)
 	{
+		for (ASearchNode SN:openList){
+			if(SN.equals(node))
+				return SN;
+		}
 		return null;
 	}
 
 	@Override
-	public boolean isOpen
-	(
-		ASearchNode node
-	) 
+	public boolean isOpen(ASearchNode node)
 	{
+		for (ASearchNode SN:openList){
+			if(SN.equals(node))
+				return true;
+		}
 		return false;
 	}
 	
 	@Override
-	public boolean isClosed
-	(
-		ASearchNode node
-	) 
+	public boolean isClosed(ASearchNode node)
 	{
+		for (ASearchNode SN: closedList){
+			if(SN.equals(node))
+				return true;
+		}
 		return false;
 	}
 
 	@Override
-	public void addToOpen
-	(
-		ASearchNode node
-	) 
+	public void addToOpen(ASearchNode node)
 	{
-
+		if(node!=null)
+			openList.add(node);
 	}
 
 	@Override
-	public void addToClosed
-	(
-		ASearchNode node
-	) 
+	public void addToClosed(ASearchNode node)
 	{
-
+		if(node!=null)
+			closedList.add(node);
 	}
 
 	@Override
@@ -82,4 +85,16 @@ public class UniformCostSearch   extends ASearch
 		return null;
 	}
 
+	public class ASearchNodeComparator implements Comparator<Object> {
+
+		public int compare(Object x, Object y) {
+			ASearchNode x1 = (ASearchNode)x;
+			ASearchNode y1 = (ASearchNode)y;
+			if (x1.getG() < y1.getG()) {
+				return -1;
+			} else {
+				return x1.getG() > y1.getG() ? 1 : 0;
+			}
+		}
+	}
 }
