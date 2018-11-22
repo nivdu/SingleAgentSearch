de5bedf8d51c9feef1f7da3d3b1f74f663b91705
nivdu <nivdu@post.bgu.ac.il>
2018-11-22 19:06:46 +0200

    BFS Done

--- src/BreadthFirstSearch.java
+++ src/BreadthFirstSearch.java
@@ -1,8 +1,10 @@
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class BreadthFirstSearch  extends ASearch
 {
-	// Define lists here ...
-	
+	Queue<ASearchNode> openList;
+	Queue<ASearchNode> closeList;
 	@Override
 	public String getSolverName() 
 	{
@@ -22,64 +24,60 @@ public class BreadthFirstSearch  extends ASearch
 	@Override
 	public void initLists() 
 	{
-
+		openList = new LinkedList<>();
+		closeList = new LinkedList<>();
 	}
 
 	@Override
-	public ASearchNode getOpen
-	(
-		ASearchNode node
-	) 
-	{
+	public ASearchNode getOpen (ASearchNode node) {
+		for (ASearchNode SN:openList){
+			if(SN.equals(node))
+				return SN;
+		}
 		return null;
 	}
 
 	@Override
-	public boolean isOpen
-	(
-		ASearchNode node
-	) 
+	public boolean isOpen(ASearchNode node)
 	{
+		for (ASearchNode SN:openList){
+			if(SN.equals(node))
+				return true;
+		}
 		return false;
 	}
 	
 	@Override
-	public boolean isClosed
-	(
-		ASearchNode node
-	) 
-	{
+	public boolean isClosed(ASearchNode node) {
+		for (ASearchNode SN:closeList){
+			if(SN.equals(node))
+				return true;
+		}
 		return false;
 	}
 
 	@Override
-	public void addToOpen
-	(
-		ASearchNode node
-	) 
-	{
-		
+	public void addToOpen(ASearchNode node) {
+		if(node!=null)
+			openList.add(node);
 	}
 
 	@Override
-	public void addToClosed
-	(
-		ASearchNode node
-	) 
-	{
- 
+	public void addToClosed(ASearchNode node) {
+		if(node!=null)
+			closeList.add(node);
 	}
 
 	@Override
 	public int openSize() 
 	{
-		return 0;
+		return openList.size();
 	}
 
 	@Override
 	public ASearchNode getBest() 
 	{
-		return null;
+		return openList.remove();
 	}
 
 	
