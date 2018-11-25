08e863f97bebeb488fa28da650927b858c24bed2
nivdu <nivdu@post.bgu.ac.il>
2018-11-25 16:40:54 +0200

    Phs and tile puzlle heuristic

--- src/TilePuzzleHeuristic.java
+++ src/TilePuzzleHeuristic.java
@@ -3,12 +3,23 @@ public class TilePuzzleHeuristic implements IHeuristic
 {
 
 	@Override
-	public double getHeuristic
-	(
-		IProblemState problemState
-	) 
-	{
-		return 0;
+	public double getHeuristic(IProblemState problemState) {
+		int length=0;
+		int sum=0;
+		if(problemState instanceof TilePuzzleState)
+			length = ((TilePuzzleState) problemState)._tilePuzzle.length;
+		else return 0;
+		int[][] tilePuz = ((TilePuzzleState) problemState)._tilePuzzle;
+		for (int i = 0; i < length; i++) {
+			for (int j = 0; j < length; j++) {
+				int curr = tilePuz[i][j];
+				int shouldBeRow = curr/length;
+				int distanceRow = Math.abs(shouldBeRow-i);
+				int shouldBeCol = (curr%length)-1;
+				int distanceCol = Math.abs(shouldBeCol - i);
+				sum += (distanceCol+distanceRow)*curr;
+			}
+		}
+		return sum;
 	}
-	
 }
